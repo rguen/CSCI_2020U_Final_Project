ebc1353e984c28349e3a46547e4a5e8a7afdc791
Simon Lee <simon.lee2@ontariotechu.net>
2020-03-25 21:57:44 -0400

    Basically finished all the logic but no images yet

--- src/Piece.java
+++ src/Piece.java
@@ -8,10 +8,16 @@ public class Piece {
     private int length;
     boolean isHorizontal = true;
     boolean[] hitbox;
+    int[] xHitbox = new int[];
+    int[] yHitbox = new int[];
+
     public Piece(int length) {
         width = 1;
         this.length = length;
-        boolean [] hitbox = new boolean[this.length];
+        this.hitbox = new boolean[this.length];
+        this.xHitbox = new int[this.length];
+        this.yHitbox = new int[this.length];
+
         //use images here
     }
 
@@ -41,20 +47,7 @@ public class Piece {
         return true;
     }
 
-    //uses the detection of where the player clicks and updates the hitbox appropriately
-    public void hitDetect() {
-        if(board[i][j] == "Hit") {
-
-        }
-    }
-
-    //places the hit boxes for the ships appropriately where the player clicks
-    //returns the success value of if the ship was successfully placed or not
-    /*
-    * How to bind hit box with specific board spot
-    * HOW TO GET BOARDSIZE
-    *
-    * */
+    //returns the success value of if the ship was successfully placed
     public boolean checkValid(int x, int y, Rectangle[][] board) {
         //checking to see if the ship goes over the edges of the board
         if (isHorizontal && hitbox.length + x > Board.BOARD_SIZE) {
@@ -83,6 +76,42 @@ public class Piece {
         //if none of the previous conditions are violated the piece can successfully be placed
         return true;
     }
+
     //directly link hitbox 1 to 1 with board space
-    public void placePiece()
+    //if [2][2] is hit ship2.gethit(2,2)
+    public void placePiece(int x, int y, Board board){
+        Rectangle[][] pBoard = board.getBoard();
+        if(isHorizontal) {
+            for (int j = x; j < x + this.length; j++) {
+                pBoard[j][y].setId("Ship");
+                xHitbox[j - x] = j;
+                yHitbox[j - x] = y;
+            }
+        }
+        else {
+            for (int k = y; k < y + this.length; k++) {
+                pBoard[x][k].setId("Ship");
+                xHitbox[k - y] = x;
+                yHitbox[k - y] = k;
+            }
+        }
+    }
+
+
+    //NOTE THIS IS SUPER INEFFICIENT YOU HAVE TO CALL THIS METHOD FOR EVERY SHIP UNTIL YOU FIND THE PIECE HIT
+    //scan the board to check for any hits on the ship
+    //returns if the specific hitbox has been found
+    public boolean markHit(int x, int y) {
+        for(int j = 0; j < Board.BOARD_SIZE; j++) {         //looping through the whole board
+            for(int k = 0; k < Board.BOARD_SIZE; k++) {     //looping through the whole board
+                for (int i = 0; i < this.length; i++){      //looping through the individual ship hitboxes
+                    if(j == xHitbox[i] && k == yHitbox[i]){
+                        hitbox[i] = true;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
